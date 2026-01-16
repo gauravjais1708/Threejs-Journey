76b91a66b640e7cfa80b64e22d4533120531563a
Gaurav jaiswal <gauravjayswal17082002@gmail.com>
2026-01-16 19:33:18 +0530

    changess

--- 05-animations/src/script.js
+++ 05-animations/src/script.js
@@ -11,7 +11,7 @@ const scene = new THREE.Scene()
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000,wireframe:true })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
@@ -23,7 +23,8 @@ const sizes = {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+camera.position.z = 2
+camera.lookAt(mesh.position)
 scene.add(camera)
 
 // Renderer
